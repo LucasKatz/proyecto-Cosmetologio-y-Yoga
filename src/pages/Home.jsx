744e46cf33563e3926484d5a8c03fa5217005f0e
Fabian Da Silva <fabian.da.silva@hotmail.com>
2023-01-20 09:55:42 -0300

    Base

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+ 
+import { Footer, Nav } from '../components';
+ 
+
+
+function Home() {
+  return (
+    <div>       
+       <h1>Aca van los components</h1>
+      <Nav/>
+       <Footer/>
+    </div>
+  )
+}
+
+export default Home
