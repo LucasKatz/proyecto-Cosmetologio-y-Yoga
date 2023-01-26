54d9871741247181f38bcba8790858a7e466bca0
Rocío Rivadeneira <rociorivadeneira2@gmail.com>
2023-01-26 10:29:36 -0300

    pp

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
-import  Home  from './pages'
-import Clases from './pages'
-import Bio from './pages'
-import Info from './pages'
-import Contacto from './pages'
-import {NavBar} from './components'
-import {Footer} from './components'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../src/styles/style.css'
- 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { Home } from "./pages";
+import "../src/styles/style.css";
+
 function App() {
- 
   return (
     <div className="App">
-        <BrowserRouter>
-      <NavBar/> 
-      <Routes>
-          < Route path = '/' element= {<Home/>} />
-          < Route path ='/clases' element={<Clases/>} />
-          < Route path = '/bio' element= {<Bio/>}/>
-          < Route path= '/info' element={<Info/>}/>
-          < Route path='/contacto' element={<Contacto/>}/> 
-          < Route path='*' element={<h1>404 NOT FOUND</h1>} />
-      </Routes>
-      <Footer/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
