41814f7dbdb98f99b9a401ec46edceaa9f4aac75
Lucas Katz <l.katz92@gmail.com>
2023-01-25 23:06:21 -0300

    last Lucas

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 import "../components/Nav.css"
 
 
@@ -12,11 +13,11 @@ const NavBAr = () => {
           <Navbar.Brand  href="#home"><img className='logo' src="https://res.cloudinary.com/dew1za4wz/image/upload/v1674251802/plants/logo_ktpfxw.png" alt="" /></Navbar.Brand>
           <Nav className="me-auto">
             <div className='contenedorLinks'>
-            <Nav.Link className='Link' href="#home">Home</Nav.Link>
-            <Nav.Link className='Link' href="#features">Clases</Nav.Link>
-            <Nav.Link className='Link' href="#pricing">Bio</Nav.Link>
-            <Nav.Link className='Link' href="#pricing">Info</Nav.Link>
-            <Nav.Link className='Link' href="#pricing">Contacto</Nav.Link>
+            <NavLink className='Link' to = '/'>Home</NavLink>
+            <NavLink className='Link'  to = '/'>Clases</NavLink>
+            <NavLink className='Link' to = '/'>Bio</NavLink>
+            <NavLink className='Link'  to = '/'>Info</NavLink>
+            <NavLink className='Link'  to = '/'>Contacto</NavLink>
             </div>
           </Nav>
         </Container>
@@ -40,8 +41,8 @@ export default NavBAr
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav classNameName="me-auto">
       <NavDropdown title="Menu" id="basic-nav-dropdown">
-      <Nav.Link classNameName="link"href="#home">Home</Nav.Link> <br />
-      <Nav.Link classNameName="link"href="#link">Clases</Nav.Link><br />
+      <NavLink classNameName="link"href="#home">Home</NavLink> <br />
+      <NavLink classNameName="link"href="#link">Clases</NavLink><br />
         <NavDropdown.Item classNameName="link" href="#action/3.1">Bio</NavDropdown.Item><br />
         <NavDropdown.Item classNameName="link"href="#action/3.2">
           Info
