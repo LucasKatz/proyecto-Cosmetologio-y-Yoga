f684c2ccd080558e7e220db4dd4aef7bc92765a5
Lucas Katz <l.katz92@gmail.com>
2023-03-13 08:08:57 -0300

    ultimando detalles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,32 @@
 import { faInstagram, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import BodyAndMindLogo from "../assets/logo_footer.png";
+import "./Footer.css";
+
 const Footer = () => {
   return (
     <footer>
-      <div className="social-media">
-        <div className="icon">
-          <img
-            alt=""
-            className="icon_img"
-            src="https://res.cloudinary.com/dew1za4wz/image/upload/v1674251802/plants/logo_ktpfxw.png"
-            srcSet=""
-          />
-        </div>
-        <div className="rrss">
-          <a href="https://www.instagram.com/holistic.bodyandmind/" rel="noopener noreferrer" target="_blank">
-            <FontAwesomeIcon className="rrss_social-icon" icon={faInstagram} />
+      <div className="container h-100 d-flex justify-content-between align-items-center">
+        <img alt="Body & Mind" src={BodyAndMindLogo} />
+        <div className="w-auto media">
+          <a
+            href="https://www.instagram.com/holistic.bodyandmind/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon alt="Instagram icon" icon={faInstagram} />
           </a>
-          <a href="https://api.whatsapp.com/send?phone=5493434667572"rel="noopener noreferrer" target="_blank">
-            <FontAwesomeIcon className="rrss_social-icon" icon={faWhatsapp} />
+          <a
+            href="https://api.whatsapp.com/send?phone=5493434667572"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              alt="Facebook icon"
+              className="ms-4"
+              icon={faWhatsapp}
+            />
           </a>
         </div>
       </div>
