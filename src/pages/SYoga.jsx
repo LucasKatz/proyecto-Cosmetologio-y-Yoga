ab47011a1263bdbe0bdc9e26655292919b3e5e1b
Rocío Rivadeneira <rociorivadeneira2@gmail.com>
2023-03-17 11:21:29 -0300

    size Video

--- src/pages/SYoga.jsx
+++ src/pages/SYoga.jsx
@@ -5,42 +5,42 @@ function SYoga() {
   return (
     <>
       <section className="hero">
-        <video autoPlay controls loop className="hero__video">
+        <video loop className="w-100" controls="false">
           <source src="https://res.cloudinary.com/duqn8v8te/video/upload/v1678291950/Dise%C3%B1o_sin_t%C3%ADtulo_dawm24.mp4" />
         </video>
       </section>
       {/* Esta section rompe revisar */}
       {/* <section className="row justify-content-center align-items-center"> */}
-        <div
-          className="parrafoSY w-100 pt-5"
-          style={{ width: "51rem", fontSize: "1.8rem" }}
+      <div
+        className="parrafoSY w-100 pt-5"
+        style={{ width: "51rem", fontSize: "1.8rem" }}
+      >
+        <p
+          className="parrafo m-4 pt-5 text-center w-50"
+          style={{ color: "white" }}
         >
-          <p
-            className="parrafo m-4 pt-5 text-center w-50"
-            style={{ color: "white" }}
-          >
-            <strong className="textoVideoYoga">
-              {" "}
-              “A través del yoga, la alimentación saludable y el cuidado del
-              cuerpo, descubrí una conexión más profunda.”
-            </strong>
-          </p>
+          <strong className="textoVideoYoga">
+            {" "}
+            “A través del yoga, la alimentación saludable y el cuidado del
+            cuerpo, descubrí una conexión más profunda.”
+          </strong>
+        </p>
 
-          <div className="contenedorBody">
-            <Button className="boton" style={{ backgroundColor: "#C87E5D" }}>
-              <a
-                className="href"
-                href="https://api.whatsapp.com/send?phone=393398228987"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <span className="textoBoton mx-auto">
-                  Reserva tu clase de prueba!
-                </span>
-              </a>
-            </Button>
-          </div>
+        <div className="contenedorBody">
+          <Button className="boton" style={{ backgroundColor: "#C87E5D" }}>
+            <a
+              className="href"
+              href="https://api.whatsapp.com/send?phone=393398228987"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <span className="textoBoton mx-auto">
+                Reserva tu clase de prueba!
+              </span>
+            </a>
+          </Button>
         </div>
+      </div>
       {/* </section> */}
     </>
   );
